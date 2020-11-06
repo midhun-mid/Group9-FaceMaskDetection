a82b2dc685d37c3afae2ad3af265390088b7615a
Bobin <bobinm3@gmail.com>
2020-11-06 20:30:44 +0530

    minor updations to code

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -27,7 +27,7 @@ INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
-DIRECTORY = "#Add directory path of dataset upon completion"
+DIRECTORY = "" #*****Add directory path of dataset upon completion and mention the path in documentation/README.mk file****
 CATEGORIES = ["with_mask", "without_mask", "improper_mask"]
 
 # grab the list of images in our dataset directory, then initialize
@@ -40,10 +40,10 @@ labels = []
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
     for img in os.listdir(path):
-    img_path = os.path.join(path, img)
-    image = load_img(img_path, target_size=(224, 224))
-    image = img_to_array(image)
-    image = preprocess_input(image)
+        img_path = os.path.join(path, img)
+        image = load_img(img_path, target_size=(224, 224))
+        image = img_to_array(image)
+        image = preprocess_input(image)
 
     data.append(image)
     labels.append(category)
