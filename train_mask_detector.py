04bf77dd89b463e2a0a72cd981a0bcfd0bba056b
shamini-sreekumar <62601777+shamini-sreekumar@users.noreply.github.com>
2020-11-28 10:37:48 +0530

    classification report

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -114,3 +114,12 @@ predIdxs = model.predict(testX, batch_size=BS)
 # for each image in the testing set we need to find the index of the
 # label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
+
+
+# show a nicely formatted classification report
+print(classification_report(testY.argmax(axis=1), predIdxs,
+target_names=lb.classes_))
+
+# serialize the model to disk
+print("[INFO] saving mask detector model...")
+model.save("mask_detector.model", save_format="h5")
