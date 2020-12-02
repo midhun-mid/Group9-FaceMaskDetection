b866242dc8a842082fcd0e5943e3c2d9a6a8139c
Bobin Mathew <32351527+BobinMathew@users.noreply.github.com>
2020-12-02 20:30:25 +0530

    Detect faces in the frame

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -13,3 +13,14 @@ import os
 prototxtPath = ""
 weightsPath = r"face_detector/res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# loop over the frames from the video stream
+while True:
+  # grab the frame from the threaded video stream and resize it
+  # to have a maximum width of 400 pixels
+  frame = vs.read()
+  frame = imutils.resize(frame, width=400)
+
+  # detect faces in the frame and determine if they are wearing a
+  # face mask or not
+  (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
